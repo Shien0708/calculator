346e6a5dd0892759e2682610345783286cb30d96
Shien <f0928652413@gmail.com>
2022-01-12 17:30:47 +0800

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by 方仕賢 on 2022/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
